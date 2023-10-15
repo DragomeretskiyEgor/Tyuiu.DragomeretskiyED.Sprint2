c8c5fc14f37ef9f9527ceab981a6df910b6abe64
Tyuiu.DragomeretskiyED@outlook.com <mrtvinksi@mail.ru>
2023-10-15 19:23:59 +0500

    Task4

---
+++ Tyuiu.DragomeretskiyED.Sprint2.Task4.V15.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.DragomeretskiyED.Sprint2.Task4.V15.Lib;
+
+namespace Tyuiu.DragomeretskiyED.Sprint2.Task4.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 5;
+            double res = ds.Calculate(x);
+            double wait = 5.3;
+            Assert.AreEqual(wait, res);
+        }
+
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 15;
+            double res = ds.Calculate(x);
+            double wait = 2.766;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DragomeretskiyED.Sprint2.Task4.V15/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.DragomeretskiyED.Sprint2.Task4.V15.Lib;
+
+namespace Tyuiu.DragomeretskiyED.Sprint2.Task4.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнил: Драгомерецкий Егор Дмитриевич | СМАРТб-23-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение                         *");
+            Console.WriteLine("* и печатает результат на экран                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadKey();
+
+        }
+    }
+}
